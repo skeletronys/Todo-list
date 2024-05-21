bb87c98518d8f773e7b824690191c5d0b8dd9553
skeletronys <mykytcey@gmail.com>
2024-05-21 15:48:17 +0200

    Update: settings add boostrap

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'crispy_bootstrap4',
     'togo_list',
 ]
 
@@ -69,6 +70,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
