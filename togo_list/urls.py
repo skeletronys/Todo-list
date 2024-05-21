c627b1bcf00944af688fda641afb90a0f2bd19f4
skeletronys <mykytcey@gmail.com>
2024-05-21 14:54:57 +0200

    Update: add TagsListView, and add to urls.py

--- togo_list/urls.py
+++ togo_list/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+
+from .views import TagsListView
+
+urlpatterns = [
+    path("", TagsListView.as_view(), name="tag-list-view")
+]
--- togo_list/views.py
+++ togo_list/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from togo_list.models import Tag
+
+
+class TagsListView(generic.ListView):
+    model = Tag
+    context_object_name = "tag_list"
+    template_name = "togo/tag_list.html"
